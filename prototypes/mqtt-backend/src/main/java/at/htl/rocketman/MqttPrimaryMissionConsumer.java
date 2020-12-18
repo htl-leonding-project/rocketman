a277e444698a4f0f9704febbd063202f886b8a03
Jan Kaufmann <jan.kaufmann@liwest.at>
2020-12-18 10:09:09 +0100

    fix date in db, fix error

--- prototypes/mqtt-backend/src/main/java/at/htl/rocketman/MqttPrimaryMissionConsumer.java
+++ prototypes/mqtt-backend/src/main/java/at/htl/rocketman/MqttPrimaryMissionConsumer.java
@@ -3,21 +3,21 @@ package at.htl.rocketman;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.jboss.logging.Logger;
 
-import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.RequestScoped;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-@ApplicationScoped
+@RequestScoped
 public class MqttPrimaryMissionConsumer {
 
     private static final Logger LOG = Logger.getLogger(MqttPrimaryMissionConsumer.class);
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
 
     @Incoming("temperature")
-    public void consumeTemperature(byte[] raw) throws SQLException {
+    public void consumeTemperature(byte[] raw) {
         Datasource ds = new Datasource();
         Double temp = null;
         try {
@@ -25,7 +25,6 @@ public class MqttPrimaryMissionConsumer {
         } catch (Exception e) {
             LOG.error("Failed to parse value");
         }
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
         if(temp != null) {
             LOG.info("Temperature: " + temp + "; " + LocalDateTime.now().format(formatter));
             //ExportController.writeToCsv(new String[] {temp.toString(), LocalDateTime.now().toString()}, "temperature.csv");
@@ -33,20 +32,21 @@ public class MqttPrimaryMissionConsumer {
                 Connection conn = ds.getDb();
                 LOG.info("Connected.");
                 PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO temperature VALUES (?, ?)");
-                preparedStatement.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now().format(formatter)));
+                preparedStatement.setString(1, LocalDateTime.now().format(formatter));
                 preparedStatement.setDouble(2, temp);
                 preparedStatement.executeUpdate();
-                conn.close();
+                //conn.close();
             } catch (SQLException e) {
-                LOG.error("SQl-Error occured: " + e.getMessage());
+                LOG.error("SQl-Error occurred: " + e.getMessage());
             } catch (Exception e) {
-                LOG.error("Unknown error occured: " + e.getMessage());
+                LOG.error("Unknown error occurred: " + e.getMessage());
             }
         }
     }
 
     @Incoming("atmospheric-pressure")
     public void consumeAtmosphericPressure(byte[] raw) {
+        Datasource ds = new Datasource();
         Float pressure = null;
         try {
             pressure = Float.parseFloat(new String(raw));
@@ -55,7 +55,19 @@ public class MqttPrimaryMissionConsumer {
         }
         if(pressure != null) {
             LOG.info("Pressure: " + pressure + "; " + LocalDateTime.now());
-            ExportController.writeToCsv(new String[] {pressure.toString(), LocalDateTime.now().toString()}, "atmospheric-pressure.csv");
+            try {
+                Connection conn = ds.getDb();
+                LOG.info("Connected.");
+                PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO atmospheric_pressure VALUES (?, ?)");
+                preparedStatement.setString(1, LocalDateTime.now().format(formatter));
+                preparedStatement.setDouble(2, pressure);
+                preparedStatement.executeUpdate();
+                //conn.close();
+            } catch (SQLException e) {
+                LOG.error("SQl-Error occurred: " + e.getMessage());
+            } catch (Exception e) {
+                LOG.error("Unknown error occurred: " + e.getMessage());
+            }
         }
     }
 }
