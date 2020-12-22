c510fddfca3bdebb447afbab78573ec87f43c30d
Jan Kaufmann <jan.kaufmann@liwest.at>
2020-12-22 13:26:48 +0100

    add loggerproducer

---
+++ prototypes/mqtt-backend/src/main/java/at/htl/rocketman/repository/LoggerProducer.java
@@ -0,0 +1,13 @@
+package at.htl.rocketman.repository;
+
+import org.jboss.logging.Logger;
+
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+
+public class LoggerProducer {
+    @Produces
+    public Logger produceLogger(InjectionPoint injectionPoint) {
+        return Logger.getLogger(injectionPoint.getBean().getBeanClass());
+    }
+}
