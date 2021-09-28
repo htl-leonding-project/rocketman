610e389ef5a964bee74e212aaee5d9d8bb350ca8
Lukas Hain <lukashain9@gmail.com>
2021-09-28 10:04:49 +0200

    bugfix in CanSatConfigurationRepository #53

--- prototypes/mqtt-backend/src/main/java/at/htl/rocketman/repository/CanSatConfigurationRepository.java
+++ prototypes/mqtt-backend/src/main/java/at/htl/rocketman/repository/CanSatConfigurationRepository.java
@@ -31,7 +31,6 @@ public class CanSatConfigurationRepository {
             sb.append(config.getCountdown()).append(";");
             sb.append(config.getIgniter()).append(";");
             sb.append(config.getResistance()).append(";");
-            sb.append(config.isUseJoyStick()).append(";");
             sb.append(config.isUseVideo()).append("\n");
 
             Files.write(Paths.get(FILENAME), sb.toString().getBytes(), StandardOpenOption.APPEND);
@@ -42,7 +41,7 @@ public class CanSatConfigurationRepository {
 
     public List<CanSatConfiguration> readConfig() {
         File file = new File(FILENAME);
-        List<CanSatConfiguration> res = new LinkedList<CanSatConfiguration>();
+        List<CanSatConfiguration> res = new LinkedList<>();
         List<String> lines = null;
         try {
             lines = Files.readAllLines(file.toPath());
