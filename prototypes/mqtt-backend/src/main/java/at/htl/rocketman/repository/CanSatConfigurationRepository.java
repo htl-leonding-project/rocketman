62921fe0430af28cbe41d1986be4a634c7f1a1a8
LukasHain <lukashain9@gmail.com>
2021-11-19 12:50:08 +0100

    fixed error where config GET endpoint returns undefined when file does not exist #rocket-14

--- prototypes/mqtt-backend/src/main/java/at/htl/rocketman/repository/CanSatConfigurationRepository.java
+++ prototypes/mqtt-backend/src/main/java/at/htl/rocketman/repository/CanSatConfigurationRepository.java
@@ -16,14 +16,8 @@ public class CanSatConfigurationRepository {
     final String FILENAME = "config.csv";
 
     public void writeConfig(CanSatConfiguration config) {
-        File file = new File(FILENAME);
-        try {
-            if (file.createNewFile()) {
-                Files.write(Paths.get(FILENAME), "name;countdown;igniter;resistance;useJoyStick;useVideo\n".getBytes(), StandardOpenOption.APPEND);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        File file = initFile();
+
         try  {
             StringBuilder sb = new StringBuilder();
 
@@ -41,13 +35,14 @@ public class CanSatConfigurationRepository {
     }
 
     public List<CanSatConfiguration> readConfig() {
-        File file = new File(FILENAME);
+        File file = initFile();
         List<CanSatConfiguration> res = new LinkedList<>();
         List<String> lines = null;
         try {
             lines = Files.readAllLines(file.toPath());
         } catch (IOException e) {
             e.printStackTrace();
+            return res;
         }
         lines = lines.stream().skip(1).collect(Collectors.toList());
         for (String line : lines) {
@@ -55,5 +50,17 @@ public class CanSatConfigurationRepository {
             res.add(new CanSatConfiguration(e[0], Integer.parseInt(e[1]), Integer.parseInt(e[2]), e[3], Boolean.parseBoolean(e[4]), Boolean.parseBoolean(e[5])));
         }
         return res;
+    }
+
+    private File initFile() {
+        File file = new File(FILENAME);
+        try {
+            if (file.createNewFile()) {
+                Files.write(Paths.get(FILENAME), "name;countdown;igniter;resistance;useJoyStick;useVideo\n".getBytes(), StandardOpenOption.APPEND);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return file;
     }
 }
